0c9dd2c61c919f5fdabb398988161dfec0b6d2c0
Barbora Doslikova <dev@barboradoslikova.com>
2015-12-28 15:30:59 -0700

    Update scrape.py
    - works for all author's articles, not just the first 20
    - uses beautiful soup, not lxml
    - but is too slow

--- scrape.py
+++ scrape.py
@@ -3,37 +3,65 @@
 Created on Thu Nov 26 20:14:49 2015
 
 This script extracts journal article titles (as strings in a list) 
-by an author's name from PubMed.
+by an author's name from the PubMed website.
 For a future use in a Markov Chain.
 
 @author: Barbora Doslikova
 """
 
-from lxml import html
-import requests
+import urllib
+from bs4 import BeautifulSoup
 
-# Loads the entire page from pubmed.com
-# searching for in this case the author Marks DM
-page = requests.get("http://www.ncbi.nlm.nih.gov/pubmed/?term=marks+dm")
-tree = html.fromstring(page.content)
-all_content = tree.xpath("//a[@href]/text()")
+mysearch = "marks+dm" # The searched author's name and initials
 
-# Extracts the middle of the page with the article titles mixed in with
-# "Free PMC Article" and "Similar articles" to be excluded
-trimmed_content = all_content[221:266]
-exclude = ["Free PMC Article", "Similar articles"]
+### 1. Get the count i.e. the number of articles of the searched author.
+############
+searchURL = "http://www.ncbi.nlm.nih.gov/pubmed/?term=" + mysearch + "%5Bauthor%5D" # The www to search
+page = urllib.urlopen(searchURL) # Opens the page
+pagedata = page.read() # Reads the page's html
+soup = BeautifulSoup(pagedata, 'html.parser') # Proper parsing of the html
+count_string = soup.find(class_="result_count left").text # e.g. "Items: 1 to 20 of 39"
 
-def cleaning(my_list):
-    """Takes a list of strings (trimmed_content),
-    removes the items stored in the variable (exclude),
-    returns only the article titles.
-    """  
-    clean = []
+# The no. of articles by the searched author
+# e.g. "39" as integer not unicode
+count = int(count_string.split().pop(-1)) 
+
+### 2. Get the list of all the artiles' IDs
+############
+retmax = count
+searchURLbase = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=" + str(retmax) + "&term="
+searchURL = searchURLbase + mysearch
+
+page = urllib.urlopen(searchURL) # Opens the page
+pagedata = page.read() # Reads the page
+soup = BeautifulSoup(pagedata, 'html.parser')
+pre_idlist = soup.esearchresult.idlist.text.split() # List of unicode
+
+def conv(my_list):
+    """Converts a list of unicode to
+    a list of strings.
+    """
+    idlist = []
+    for each in my_list:    
+        idlist.append(str(each))
+    return idlist
+
+idlist = conv(pre_idlist) # List of strings
+
+### 3. Use the article IDs to extract the journal article titles.
+############
+searchURLbase = "http://www.ncbi.nlm.nih.gov/pubmed/?term="
+def return_titles(my_list):
+    titles = []
     for each in my_list:
-        if each in exclude:
-            pass
-        else:
-            clean.append(each)
-    return clean
-
-print cleaning(trimmed_content)
+        searchURL = searchURLbase + each
+        page = urllib.urlopen(searchURL) # Opens the page
+        pagedata = page.read() # Reads the page
+        soup = BeautifulSoup(pagedata, 'html.parser')
+        h1 = soup.find(class_="rprt abstract").find("h1").text
+        title = str(h1)
+        titles.append(title)
+    return titles
+        
+titles = return_titles(idlist)
+print titles
