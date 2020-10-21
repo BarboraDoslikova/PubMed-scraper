30c40621bbacbfe64505d043ced11f1c2ae9149a
Barbora Doslikova <dev@barboradoslikova.com>
2020-10-21 16:05:29 +0200

    Update scrape.py
    change import urllib to import urllib.request

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 26 20:14:49 2015
+Updated on Wed Oct 21 16:04:00 2020
 
 This script extracts journal article titles (as strings in a list) 
 by an author's name from the PubMed website.
@@ -9,17 +10,17 @@ For a future use in a Markov Chain.
 @author: Barbora Doslikova
 """
 
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
-mysearch = "marks+dm" # The searched author's name and initials
+mysearch = "doslikova+b" # The searched author's name and initials
 
 def get_count(mysearch):
     """Get the count i.e. the number of articles of the searched author.
     """
     mysearch = mysearch
     searchURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1&term=" + mysearch # The www to search
-    page = urllib.urlopen(searchURL) # Opens the page
+    page = urllib.request.urlopen(searchURL) # Opens the page
     pagedata = page.read() # Reads the page's html
     soup = BeautifulSoup(pagedata, 'html.parser') # Proper parsing of the html
     count = soup.count.text # The no. of articles by the searched author as unicode
@@ -30,7 +31,7 @@ def get_ids(count):
     """
     retmax = count
     searchURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=" + str(retmax) + "&term=" + mysearch
-    page = urllib.urlopen(searchURL) # Opens the page
+    page = urllib.request.urlopen(searchURL) # Opens the page
     pagedata = page.read() # Reads the page
     soup = BeautifulSoup(pagedata, 'html.parser')
     idlist = soup.esearchresult.idlist.text.split() # List of unicode
@@ -43,7 +44,7 @@ def return_titles(my_list):
     titles = []
     for each in my_list:
         searchURL = searchURLbase + each
-        page = urllib.urlopen(searchURL) # Opens the page
+        page = urllib.request.urlopen(searchURL) # Opens the page
         pagedata = page.read()
         soup = BeautifulSoup(pagedata, 'html.parser')
         title = str(soup.articletitle.text)
@@ -56,4 +57,4 @@ def get_titles_by_author(mysearch):
     titles = return_titles(idlist)
     return titles
 
-print get_titles_by_author(mysearch)
+print(get_titles_by_author(mysearch))
